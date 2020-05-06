b8a1e2e16b0e5084f3d6e5d430ebeffff11928d7
Ryan Wilson <rwils006@ucr.edu>
2020-05-05 17:12:59 -0700

    finished some node functions and put a blueprint for finding possible states

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -6,6 +6,10 @@ public class Node {
     private Node parent; //pointer to the parent node
     private ArrayList<Node> children; //list of all the nodes children
 
+    private int cost; //cost of getting to this node g(n)
+    private int estimatedCost; //estimated cost of getting to goal h(n)
+    private int totalCost; // this is going to be cost + estimated cost, used for comparing nodes
+
     public Node(String state){ //constructor
         this.state = state;
         children = new ArrayList<Node>(); //create an empty list
@@ -20,8 +24,64 @@ public class Node {
         return this.parent;
     }
 
-    public ArrayList<Node> getChildren(){
-        return this.children;
+    public ArrayList<String> getPossibleStates(){
+        /*This Function will return all possible states you can get to from the parent node*/
+
+        ArrayList<String> possibleStates = new ArrayList<String>();
+
+        switch (this.state.indexOf('0')){ //finds where the blank is
+            case 0: { //only 2 moves if its in the upper left corner
+                //move right
+                //move down
+            }
+
+            case 1: { //upper middle
+                //move right
+                //move down
+                //move left
+            }
+
+            case 2: { //upper right
+                //move down
+                //move left
+            }
+
+            case 3: { //middle left
+                //move up
+                //move down
+                //move right
+            }
+
+            case 4: { //middle middle
+                //move up
+                //move down
+                //move left
+                //move right
+            }
+
+            case 5: { //middle right
+                //move up
+                //move down
+                //move left
+            }
+
+            case 6: { //lower left
+                //move up
+                //move right
+            }
+
+            case 7: { //lower middle
+                //move up
+                //move left
+                //move right
+            }
+
+            case 8: { //lower right
+                //move up
+                //move left
+            }
+        }
+
     }
 
     public void printNode(){
@@ -49,5 +109,25 @@ public class Node {
             }
         }
         System.out.println(']');
+    }
+
+    public void addChild(Node child){
+        this.children.add(child);
+    }
+
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
+
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+    public void setEstimatedCost(int eCost){
+        this.estimatedCost = eCost;
+    }
+
+    public void setTotalCost(){
+        this.totalCost = this.cost + this.estimatedCost;
     }
 }
--- src/com/company/Problem.java
+++ src/com/company/Problem.java
@@ -1,22 +1,41 @@
 package com.company;
-import java.util.*;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Problem {
 
-    Node current;
-    String goal;
+    private Node initialState;
+    private String goalState;
+    private ArrayList<Node> frontier; //will have to sort this to make it a priority queue
+    private HashSet<String> explored; //hash set is a fast way to store explored nodes with no duplicates
 
     public Problem(){
-        this.current = new Node("123456780"); //trivial case
-        this.goal = "123456780"; //normal goal
+        this.initialState = new Node("123456780"); //trivial case
+        this.goalState = "123456780"; //normal goal
+        this.frontier = new ArrayList<Node>();
+        this.explored = new HashSet<String>();
     }
 
     public Problem(String root){
-        this.current = new Node(root);
-        this.goal = "123456780";
+        this.initialState = new Node(root);
+        this.goalState = "123456780";
+        this.frontier = new ArrayList<Node>();
+        this.explored = new HashSet<String>();
     }
 
-    public void printCurrent(){
-        this.current.printNode();
+    public void search(int heuristic){
+        /*
+        this is the function used to search through the tree and get the solution
+        this function takes in a heuristic value that determines the value for estimated cost h(n)
+        at the end, this function prints the solution
+        */
+
+        Node node = new Node(this.initialState.getState()); //starts with root node
+
+        while(node.getState() != this.goalState){
+            this.explored.add(node.getState()); //we have visited this node
+
+        }
+
     }
 }
